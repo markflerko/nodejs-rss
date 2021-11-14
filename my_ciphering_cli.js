0bd19227ce05e4d60f852cf7dab43c522affa3a5
markflerko <mark.flerko@satelliteinnovations.io>
2021-11-14 12:03:58 +0300

    feat: add pipes

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -2,6 +2,28 @@ const fs = require("fs");
 const caesar = require("./ceasarChipher");
 const atbash = require("./atbashChipher");
 const rot8 = require("./rot8Chipher");
+const { Stream } = require("stream");
+
+const ts = new Stream.Transform();
+
+ts._transform = (chunk, encoding, cb) => {
+  chunk = chunk.toString();
+  for (let i = 0; i < config.length; i++) {
+    if (config[i] === "C1") {
+      chunk = caesar.encode(chunk);
+    } else if (config[i] === "C0") {
+      chunk = caesar.decode(chunk);
+    } else if (config[i] === "R1") {
+      chunk = rot8.encode(chunk);
+    } else if (config[i] === "R0") {
+      chunk = rot8.decode(chunk);
+    } else if (config[i] === "A") {
+      chunk = atbash(chunk);
+    }
+  }
+  ts.push(chunk);
+  cb();
+};
 
 function getFlag(flag, flagFull) {
   const flagIndex =
@@ -17,77 +39,17 @@ const outputFile = getFlag("-o", "--output");
 
 if (inputFile === null && outputFile === null) {
   console.log("please, write your message");
-  process.stdin.on("data", (data, chunk = data.toString()) => {
-    for (let i = 0; i < config.length; i++) {
-      if (config[i] === "C1") {
-        chunk = caesar.encode(chunk);
-      } else if (config[i] === "C0") {
-        chunk = caesar.decode(chunk);
-      } else if (config[i] === "R1") {
-        chunk = rot8.encode(chunk);
-      } else if (config[i] === "R0") {
-        chunk = rot8.decode(chunk);
-      } else if (config[i] === "A") {
-        chunk = atbash(chunk);
-      }
-    }
-    process.stdout.write(chunk);
-  });
+  process.stdin.pipe(ts).pipe(process.stdout);
 } else if (inputFile === null && outputFile !== null) {
   console.log("please, write your message");
-  process.stdin.on("data", (data, chunk = data.toString()) => {
-    for (let i = 0; i < config.length; i++) {
-      if (config[i] === "C1") {
-        chunk = caesar.encode(chunk);
-      } else if (config[i] === "C0") {
-        chunk = caesar.decode(chunk);
-      } else if (config[i] === "R1") {
-        chunk = rot8.encode(chunk);
-      } else if (config[i] === "R0") {
-        chunk = rot8.decode(chunk);
-      } else if (config[i] === "A") {
-        chunk = atbash(chunk);
-      }
-    }
-    const output = fs.createWriteStream(outputFile, "utf-8");
-    output.write(chunk);
-  });
+  const output = fs.createWriteStream(outputFile, "utf-8");
+  process.stdin.pipe(ts).pipe(output);
 } else if (inputFile !== null && outputFile === null) {
   const input = fs.createReadStream(inputFile, "utf-8");
-  input.on("data", (chunk) => {
-    for (let i = 0; i < config.length; i++) {
-      if (config[i] === "C1") {
-        chunk = caesar.encode(chunk);
-      } else if (config[i] === "C0") {
-        chunk = caesar.decode(chunk);
-      } else if (config[i] === "R1") {
-        chunk = rot8.encode(chunk);
-      } else if (config[i] === "R0") {
-        chunk = rot8.decode(chunk);
-      } else if (config[i] === "A") {
-        chunk = atbash(chunk);
-      }
-    }
-    process.stdout.write(chunk);
-  });
+  input.pipe(ts).pipe(process.stdout);
 } else {
   const input = fs.createReadStream(inputFile, "utf-8");
   const output = fs.createWriteStream(outputFile, "utf-8");
-  input.on("data", (chunk) => {
-    for (let i = 0; i < config.length; i++) {
-      if (config[i] === "C1") {
-        chunk = caesar.encode(chunk);
-      } else if (config[i] === "C0") {
-        chunk = caesar.decode(chunk);
-      } else if (config[i] === "R1") {
-        chunk = rot8.encode(chunk);
-      } else if (config[i] === "R0") {
-        chunk = rot8.decode(chunk);
-      } else if (config[i] === "A") {
-        chunk = atbash(chunk);
-      }
-    }
-    output.write(chunk);
-  });
+  input.pipe(ts).pipe(output);
   input.on("error", (error) => console.log("Error", error.message));
 }
