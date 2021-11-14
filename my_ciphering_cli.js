c4e5f1a1e8d4f32ebe11c6d4973f4c7839864586
markflerko <mark.flerko@satelliteinnovations.io>
2021-11-14 22:11:19 +0300

    feat: not replace output, but append new

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -8,14 +8,14 @@ const outputFile = getFlag("-o", "--output");
 if (inputFile === null && outputFile === null) {
   process.stdin.pipe(ts).pipe(process.stdout);
 } else if (inputFile === null && outputFile !== null) {
-  const output = fs.createWriteStream(outputFile, "utf-8");
+  const output = fs.createWriteStream(outputFile, { flags: 'a' });
   process.stdin.pipe(ts).pipe(output);
 } else if (inputFile !== null && outputFile === null) {
-  const input = fs.createReadStream(inputFile, "utf-8");
+  const input = fs.createReadStream(inputFile);
   input.pipe(ts).pipe(process.stdout);
 } else {
-  const input = fs.createReadStream(inputFile, "utf-8");
-  const output = fs.createWriteStream(outputFile, "utf-8");
+  const input = fs.createReadStream(inputFile);
+  const output = fs.createWriteStream(outputFile, { flags: 'a' });
   input.pipe(ts).pipe(output);
   input.on("error", (error) => console.log("Error", error.message));
 }
