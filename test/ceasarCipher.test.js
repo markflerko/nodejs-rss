1b586a8a059a0d348b377bcf550c85bf76fb8f8f
markflerko <mark.flerko@satelliteinnovations.io>
2021-11-21 17:51:39 +0300

    feat: add atbash tests

--- test/ceasarCipher.test.js
+++ test/ceasarCipher.test.js
@@ -1,6 +1,44 @@
-const caesar = require("../ceasarChipher");
+const { encode, decode } = require("../ceasarChipher");
 
-test("should have name mark", () => {
-  const string = caesar.encode('This is secret. Message about "_" symbol!');
-  expect(string).toBe('Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!');
+describe("Ceasar cipher encoding", () => {
+  test("should encrypt", () => {
+    const encoded = encode("this is secret");
+    expect(encoded).toBe("uijt jt tfdsfu");
+  });
+
+  test("should encrypt only en alphabet", () => {
+    const orig = "сообщение без англ слов со спец символами !231@";
+    const encoded = encode(orig);
+    expect(encoded).toBe(orig);
+  });
+
+  test("should keep uppercase", () => {
+    const orig = "A";
+    const code = orig.charCodeAt(0);
+    const encoded = encode(orig);
+    const codeEncoded = encoded.charCodeAt(0)
+    expect(codeEncoded).toEqual(code+1);
+  });
+});
+
+
+describe("Ceasar cipher decoding", () => {
+  test("should decrypt", () => {
+    const decoded = decode("uijt jt tfdsfu");
+    expect(decoded).toBe("this is secret");
+  });
+
+  test("should decrypt only en alphabet", () => {
+    const orig = "сообщение без англ слов со спец символами !231@";
+    const decoded = decode(orig);
+    expect(decoded).toBe(orig);
+  });
+
+  test("should keep uppercase", () => {
+    const orig = "B";
+    const code = orig.charCodeAt(0);
+    const encoded = decode(orig);
+    const codeEncoded = encoded.charCodeAt(0)
+    expect(codeEncoded).toEqual(code-1);
+  });
 });
